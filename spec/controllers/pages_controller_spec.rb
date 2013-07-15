9b0af61e7a0d28aebe7626d34ad02044f5f0b1ec
Lawrence Lee <theinversesquarelaw@gmail.com>
2013-07-15 01:37:45 -0700

    add css.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -24,7 +24,7 @@ describe PagesController do
       response.should be_success
     end
     #it "should have the right title" do
-      #get 'home'
+      #get 'contact'
       #response.should have_selector(
 	#"title", :content => "Ruby on Rails Tutorial Sample App | Contact"
       #)
@@ -41,7 +41,7 @@ describe PagesController do
       response.should be_success
     end
     #it "should have the right title" do
-      #get 'home'
+      #get 'about'
       #response.should have_selector(
 	#"title", :content => "Ruby on Rails Tutorial Sample App | About"
       #)
