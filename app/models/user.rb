055a5cedc8a9bd30fbe49ebb7faebc6d7aef2917
Lawrence Lee <theinversesquarelaw@gmail.com>
2013-07-19 15:47:00 -0700

    Finished password machinery

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,5 +35,41 @@ class User < ActiveRecord::Base
 	    :confirmation   => true,
 	    :length	    => {:within => 6..40}
 
+  before_save :encrypt_password
+
+  def has_password?(submitted_password)
+    #don't need "self." here because just a comparison
+    encrypted_password == encrypt(submitted_password)
+  end
+
+
+  #def User.authenticate == self.authenticate
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil	if user.nil?
+    return user	if user.has_password?(submitted_password)
+  end
+
+  private
+
+    def encrypt_password
+      #need "self." here b/c it will create a local variable if we don't.
+      #we want to assign to this objects encrypted_password not create a new one
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{self.salt}--#{string}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130719202327) do
+ActiveRecord::Schema.define(version: 20130719205234) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20130719202327) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
