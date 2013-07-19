78129e5e8a76a0d22cb6792fb599031f2b58d724
Lawrence Lee <theinversesquarelaw@gmail.com>
2013-07-19 13:26:37 -0700

    Added password and encrypted_password attributes to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,21 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base
+  #create a password attribute that you can access
+  attr_accessor	  :password
+  
   #security to only allow users to change name and email (other db columns not user acccesable)
-  attr_accessible :name, :email
+  attr_accessible :name, :email, :password, :password_confirmation
+
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   #\A and \z test for beginning and end of string
@@ -19,9 +24,16 @@ class User < ActiveRecord::Base
   #ensures that name and email are non-empty, and name is not longer than 50
   validates :name, 
 	    :presence	=> true,
-	    :length	=> {:maximium => 50}
+	    :length	=> {:maximum => 50}
   validates :email, 
 	    :presence	=> true,
-	    :format	=> {:with => email_regex}
+	    :format	=> {:with => email_regex},
 	    :uniqueness => {:case_sensitive => false}
+
+  validates :password,
+	    :presence	    => true,
+	    :confirmation   => true,
+	    :length	    => {:within => 6..40}
+
 end
+
