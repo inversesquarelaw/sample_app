0debc79d24cef73b206b3f3d5272a193ec1339cd
Lawrence Lee <theinversesquarelaw@gmail.com>
2013-07-21 18:58:40 -0700

    Finished lesson 8: signup failure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,14 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #Handle a successful save.
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+
 end
