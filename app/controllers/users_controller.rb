bb56cd0408226bb784e3131648fc0e94922fb698
Lawrence Lee <theinversesquarelaw@gmail.com>
2013-07-21 21:54:39 -0700

    Finished lesson 8 conclusion. But skipped the rspec integration tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      #Handle a successful save.
+      redirect_to @user, :flash => {:success => "Welcome to the Sample App!"}
     else
       @title = "Sign up"
       render 'new'
